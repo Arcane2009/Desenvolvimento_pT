d19f6972913e139ace1414bad45f1940ee547a38
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2026-02-27 10:45:13 -0300

    Add files via upload

---
+++ prestidigitacao_game/var_test.py
@@ -0,0 +1,16 @@
+#print("Olá mundo!")
+
+#variavel1 = "Uma variável variada"
+#variavel2 = 500
+#variavel3 = 20.5
+#variavel4 = True
+#print(type(variavel1),type(variavel2),type(variavel3),type(variavel4))
+
+#input("Digite um valor aleatório: ")#
+
+#for teste in range(1,10, 3)#
+
+import random as rd
+
+testeRd = rd.randrange(0, 101)
+print(testeRd)
