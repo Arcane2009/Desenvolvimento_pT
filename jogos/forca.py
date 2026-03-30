f3bc2c355e2da498314dc26293dbb40bc082116c
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2026-03-30 11:24:41 -0300

    Add files via upload

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,22 +3,33 @@ def jogar_forca():
     print("\nBem vindo ao jogo da forca!\n")
     print("-------------------------------")
 
-    palavra_secreta = "cordeiro"
+    lista = []
+
+    palavra_secreta = "cordeiro".lower()
+    letras_acertadas = ["_","_","_","_","_","_","_","_"]
     perdeu = False
     acertou = False
+    erros = 0
 
+    #Enquanto não acertar a palavra secreta
     while not perdeu and not acertou:
         chute = input("Escreva uma letra: ")
-        chute = chute.strip()
+        chute = chute.strip().lower()
 
         #index define a posição da letra
         index = 0
+        if chute in palavra_secreta:
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
-        for letra in palavra_secreta:
-            if chute.lower() == letra.lower():
-                print(f"A letra {chute} está na posição {index}")
-            index = index + 1
+        perdeu = erros == 6
+        acertou = "_" not in letras_acertadas
+
+        print(letras_acertadas)
 
 if(__name__ == "__main__"):
     jogar_forca()
-
--- jogos/sigiloso.py
+++ jogos/sigiloso.py
@@ -5,8 +5,21 @@
 
 #print(palavra.lower())
 #-------------------------------------------------------------------------------------------------#
-teste_valores = [2, 3, 4, 'L']
+#teste_valores = [2, 3, 4, 'L']
 
-print(teste_valores[3])
+#print(teste_valores[3])
 
 #Gere duas listas uma de numeros e outra de letras
+#-------------------------------------------------------------------------------------------------#
+#pronomes_singular = ("Eu", "Você", "Ele", "Ela")
+#pronomes_singular.append("Nós")
+#pronomes_singular.pop()
+#print(pronomes_singular)
+
+p1 = (2,3)
+p2 = (6,8)
+p3 = (10,12)
+
+linha = [p1, p2, p3]
+
+print(linha[1][1])
