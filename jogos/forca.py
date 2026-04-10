bc4565fb5711472efde0f2f83eb9e1b37ddde726
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2026-04-10 11:49:11 -0300

    Add files via upload

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,28 +1,56 @@
-def jogar_forca():
+import random as rd
+
+def mensagem_inicial():
     print("-------------------------------")
     print("\nBem vindo ao jogo da forca!\n")
     print("-------------------------------")
 
-    lista = []
+def seleciona_palavra_rd():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
 
-    palavra_secreta = "cordeiro".lower()
-    letras_acertadas = ["_","_","_","_","_","_","_","_"]
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+    posicao = rd.randrange(0,len(palavras))
+    
+    palavra_secreta = palavras[posicao].lower()
+
+    return palavra_secreta
+#0,5 de nota
+def letras_corretas(palavra_secreta):
+    return ["_" for letra in palavra_secreta]
+
+def entrada_dados():
+    chute = input("Escreva uma letra: ")
+    chute = chute.strip().lower()
+    return chute
+
+def chute_correto(palavra_secreta, chute, letras_acertadas):
+    #index define a posição da letra
+    index = 0
+    for letra in palavra_secreta:
+        if chute == letra:
+            letras_acertadas[index] = letra
+            index += 1
+            
+def jogar_forca():
+    mensagem_inicial()
+    palavra_secreta = seleciona_palavra_rd()
+    letras_acertadas = letras_corretas(palavra_secreta)
+
     perdeu = False
     acertou = False
     erros = 0
 
     #Enquanto não acertar a palavra secreta
     while not perdeu and not acertou:
-        chute = input("Escreva uma letra: ")
-        chute = chute.strip().lower()
+        chute = entrada_dados()
 
-        #index define a posição da letra
-        index = 0
         if chute in palavra_secreta:
-            for letra in palavra_secreta:
-                if chute == letra:
-                    letras_acertadas[index] = letra
-                index += 1
+            chute_correto(palavra_secreta, chute, letras_acertadas)
         else:
             erros += 1
 
--- jogos/sigiloso.py
+++ jogos/sigiloso.py
@@ -16,10 +16,18 @@
 #pronomes_singular.pop()
 #print(pronomes_singular)
 
-p1 = (2,3)
-p2 = (6,8)
-p3 = (10,12)
+#p1 = (2,3)
+#p2 = (6,8)
+#p3 = (10,12)
 
-linha = [p1, p2, p3]
+#linha = [p1, p2, p3]
 
-print(linha[1][1])
+#print(linha[1][1])
+#-------------------------------------------------------------------------------------------------#
+palavra = "banana"
+lista = []
+
+for letra in palavra:
+    lista.append("_")
+
+print(lista)
