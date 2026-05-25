233ab704d7508a3b502fd6d1b09c7d9d18eb0a57
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2026-05-25 11:29:58 -0300

    Add files via upload

--- python_obj/exec.py
+++ python_obj/exec.py
@@ -1,11 +1,15 @@
 from conta import Conta
 
-conta = Conta(535, "Ricardo", 55.0, 5000.0)
-conta1 = Conta(555, "Luana", 100.0, 1000.0)
+conta1 = Conta(535, "Ricardo", 55.0, 5000.0)
+conta2 = Conta(555, "Luana", 100.0, 1000.0)
 
-conta.extrato()
-conta.sacar(5)
-conta.extrato()
-conta.depositar(10)
-conta.extrato()
+valor = 50
+
+conta2.extrato()
+conta1.transferir(valor, conta2)
+conta1.extrato()
+conta2.extrato()
 #WuW WwW WvW WoW WxW; UwU UuU UvU UxU; VuV VvV 
+#atributo = o que têm
+#método = o que consegue fazer
+#dentro da classe => métsodo
