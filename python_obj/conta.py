06303974f4e55f48be5de408e2ebb15a84ed070c
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2026-06-01 11:34:38 -0300

    Add files via upload

--- python_obj/conta.py
+++ python_obj/conta.py
@@ -3,26 +3,32 @@ class Conta:
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
-        self.__limite = limite
+        self.__limite_especial = limite
 
+    #Declaração dos métodos (funções)
     def extrato(self):
-        print(f"Saldo: {self.__saldo} do titular {self.__titular}")
+        print(f"\nSaldo: {self.__saldo} do titular {self.__titular}")
 
     def depositar(self, valor):
-        if(valor < 0):
-            print("Valores negativos não podem ser depositados")
+        if(valor <= 0):
+            print("\nValores negativos não podem ser depositados")
         else:
             self.__saldo += valor
 
+    def __saque_permitido(self, valor_saque):
+        valor_disponivel_saque = self.__saldo + self.__limite_especial
+        return valor_saque <= valor_disponivel_saque
+    
     def sacar(self, valor):
-        if(self.__saldo < valor):
-            print("Saldo insuficiente")
-        else:
+        if(self.__saque_permitido(valor)):
             self.__saldo -= valor
+        else:
+            print(f"\nO valor {valor} passou do limite")
+            
 
     def transferir(self, valor, destino):
         if(self.__saldo < valor) or (valor < 0):
-            print("Não é possível realizar a tranferência")
+            print("\nNão é possível realizar a tranferência")
         else:
             self.sacar(valor)
             destino.depositar(valor)
@@ -38,11 +44,19 @@ class Conta:
     
     @property
     def limite(self):
-        return self.__limite
+        return self.__limite_especial
     
     @property
     def numero(self):
         return self.__numero
+    
+    @staticmethod
+    def codigo_banco():
+        return '001'
+    
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
 
     #Métodos para manipular os valores das propriedades
     @saldo.setter
@@ -51,8 +65,16 @@ class Conta:
 
     @limite.setter
     def limite(self,limite):
-        self.__limite = limite
+        self.__limite_especial = limite
 
+
+    @numero.setter
+    def numero(self,numero):
+        self.__numero = numero
+
+    @titular.setter
+    def titular(self,titular):
+        self.__titular = titular
 #atributo = o que têm
 #método = o que consegue fazer
 #dentro da classe => métsodo
@@ -61,3 +83,4 @@ class Conta:
 
 #get sempre te um return, ele é um método que pega um valor(dado)
 #set sempre tem um get, ele é um método que muda os valores obtidos
+#método istático = não depende do objeto
--- python_obj/exec.py
+++ python_obj/exec.py
@@ -4,19 +4,26 @@ conta1 = Conta(535, "Ricardo", 55.0, 1000.0)
 conta2 = Conta(555, "Luana", 100.0, 1000.0)
 
 valor = 50
+conta1.limite = 2000
 
 conta2.extrato()
 conta1.transferir(valor, conta2)
 conta1.extrato()
 conta2.extrato()
-print("\n")
+
 #WuW WwW WvW WoW WxW; UwU UuU UvU UxU; VuV VvV 
 #atributo = o que têm
 #método = o que consegue fazer
 #dentro da classe => métsodo
 
-print(conta1.saldo," é da classe: ", type(conta1.saldo))
-print("\n")
+print(f"\n{conta1.saldo} é da classe: ", type(conta1.saldo))
 
-conta1.limite = 2000
-print(conta1.limite)
+print(f"\nO limite da conta1 é: {conta1.limite}")
+
+conta2.extrato()
+conta2.sacar(1200)
+conta2.extrato()
+
+codigos = Conta.codigos_bancos()
+print(f"\nO código da conta é: {codigos['BB']}")
+
