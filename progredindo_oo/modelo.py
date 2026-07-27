8982a813cce82a1fec124919a39cca457e03c060
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2026-07-27 12:23:25 -0300

    Add files via upload

--- progredindo_oo/modelo.py
+++ progredindo_oo/modelo.py
@@ -22,15 +22,24 @@ class Programas:
     def curtida(self):
         self._curtir += 1
 
+    def imprime(self):
+        print(f"\n{self._nome} - {self.ano} - {self._curtir} Curtidas")
+
 class Filmes(Programas):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def imprime(self):
+        print(f"\n{self._nome} - {self.ano} - {self.duracao} - {self._curtir} Curtidas")
+
 class Series(Programas):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+
+    def imprime(self):
+        print(f"\n{self._nome} - {self.ano} - {self.temporadas} - {self._curtir} Curtidas")
 
 #Instanciar é salvar em uma variável
 
@@ -44,5 +53,5 @@ print(f"\nNome: {avatar.valor_nome} - Ano: {avatar.ano} - Duração: {avatar.dur
 filmes_series = [aventuras_superman, avatar]
 
 for programas in filmes_series:
-    detalhe = programas.duracao if hasattr(programas, 'duracao') else programas.temporadas
-    print(f"\n{programas.valor_nome} - {programas.ano} - {detalhe}")
+    programas.imprime()
+        
